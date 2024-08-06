f80b14bd93bfe42e071ab86379e2f1d805fc2eb6
MariaAfanasievam <afanmaria@mail.ru>
2024-08-07 00:03:31 +0300

    popap

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+function BgButton() {
+   
+   // document.getElementById('kartinka').src= 'https://avatars.mds.yandex.net/i?id=ed94111b3fa206e5723eae25a4ce377b0a5dee30-12938156-images-thumbs&n=13';
+    document.getElementById('kartinka').classList.toggle("show");
+
+}
+ 
