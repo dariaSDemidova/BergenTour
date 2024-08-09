0b6b3f10a3aae913ae092d05353471c1cfc37128
MariaAfanasievam <afanmaria@mail.ru>
2024-08-09 19:00:00 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 
 function BgButton() {
    
-   // document.getElementById('kartinka').src= 'https://avatars.mds.yandex.net/i?id=ed94111b3fa206e5723eae25a4ce377b0a5dee30-12938156-images-thumbs&n=13';
+  //  document.getElementById('kartinka').src= './assets/img/review1.png';
     document.getElementById('kartinka').classList.toggle("show");
 
 }
- 
+function BgButton2() {
+   
+    //  document.getElementById('kartinka').src= './assets/img/review1.png';
+      document.getElementById('kartinka2').classList.toggle("show");
+  
+  }
+   
